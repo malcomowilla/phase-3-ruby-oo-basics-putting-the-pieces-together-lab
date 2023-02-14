755200e2c5f91abefbaf44e68045df6cc8483a0a
malcomowilla <owillamalcom@gmail.com>
2023-02-14 13:29:02 +0300

    putting pieces together

---
+++ spec/malcom.rb
@@ -0,0 +1,23 @@
+#Learning Goals
+
+#Create basic classes using Ruby
+
+#Introduction
+# Object-oriented programming, or OOP, is an extremely useful programming paradigm in which we can organize our code according to how real-world objects might interact with one another. We can wrap properties/data and behavior up in classes, and then create instances, or individual "members", of those classes that can interact with one another.
+# One common misconception about OOP is that everything MUST model the real world. If we limit our objects to things in the real world, the limitations will start jumping out at us.
+# Imagine a phone call between 2 people. Sure, the PEOPLE are real, but what about the phone call? If we think about the phone call through OOP, we can model it too! A phone call has a caller and a receiver, a duration, and maybe even a cost_per_minute. In the real world, it's not a real thing, but in OOP IT IS!
+# In this lab, you will put together everything you've learned so far about Object Orientation in Ruby. You will be building out two classes, a Book class and a Shoe class.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
